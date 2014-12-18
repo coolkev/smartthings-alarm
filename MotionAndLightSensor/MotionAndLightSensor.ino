8dc55696b4fbf1d0c705f3f7fa05aaa33dba3a38
Kevin Lewis <kevin@lewistech.com>
2014-12-18 11:37:11 -0800

    remote sensor update

--- MotionAndLightSensor/MotionAndLightSensor.ino
+++ MotionAndLightSensor/MotionAndLightSensor.ino
@@ -1,9 +1,10 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <SimpleTimer.h>
 
 // Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS 5
-#define TEMPERATURE_PRECISION 9
+#define TEMPERATURE_PRECISION 10
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
@@ -15,11 +16,15 @@ DallasTemperature sensors(&oneWire);
 
 DeviceAddress tempDeviceAddress; // We'll use this variable to store a found device address
 
+SimpleTimer timer;
 
+long lightLevelInterval = 1000;
+long temperatureInterval = 10000; // only check temp every 30 secs
+
+int lightvals[10];
 int lightpin = A0;
 int motionpin = 4;
 int ledpin = 2;
-int temppin = 5;
 int tempground = 7;
 int tempDeviceStart = 12;
 
@@ -27,17 +32,13 @@ boolean lastMotion = false;
 long lastMotionAt = 0;
 
 int lightvalue = 0;
-long lightvalueAt = 0;
 float temperature[10];
-long temperatureAt = -10000;
-long temperatureInterval = 10000; // only check temp every 30 secs
+
 
 long ledOnAt = 0;
 boolean ledOn = false;
 long lastStatusPrint = 0;
 long statusPrintTiming = 1000;
-
-OneWire ds(temppin);  // on pin 10 (a 4.7K resistor is necessary)
 
 #include <SoftwareSerial.h>
 #include <SoftEasyTransfer.h>
@@ -51,6 +52,8 @@ struct SEND_DATA_STRUCTURE{
   //THIS MUST BE EXACTLY THE SAME ON THE OTHER ARDUINO
   byte sensor;
   int reading;
+  boolean readingNegative;
+//  char message[50];
 };
 
 //give a name to the group of data
@@ -63,7 +66,7 @@ void setup()
   Serial.begin(57600);      
   Serial.println("setup.."); 
   
-  mySerial.begin(9600);
+  mySerial.begin(2400);
   
   ET.begin(details(mydata), &mySerial);
   
@@ -74,46 +77,80 @@ void setup()
   pinMode(ledpin, OUTPUT);
   digitalWrite(ledpin, LOW);
   
-  pinMode(tempground, OUTPUT);
-  digitalWrite(tempground, LOW);
-    
   initTemperatures();
   
+  
+  timer.setInterval(lightLevelInterval, checkLightLevelBig);
+  timer.setInterval(lightLevelInterval*60, checkLightLevelSmall);
+    
+  timer.setInterval(temperatureInterval, checkTemperatures);
+  
+  String msg = "Garage sensor started ";
+  
+  msg += sensors.getDeviceCount();
+  msg += " temp sensors";
+  
+  Serial.println(msg);
+  
+  //sendData(msg);
   
 }
 
 
 void loop() {
-  
-  
-  
-  int lightvalueNew = int(analogRead(lightpin)/1024.0*100)*10;
-  
-  int lightChange = abs(lightvalueNew-lightvalue);
-  //only if changed by >= 10
-  if (lightChange>=10) {
 
-     
-     //and at most every 10 seconds or > 100 change
-     if (lightChange>=100 || (millis() - lightvalueAt) >= 10000) 
-     {
-       lightvalueAt = millis();
-       lightvalue = lightvalueNew;
-       
-       Serial.print("w 10 ");
-       Serial.println(lightvalueNew);
-       
-       mydata.sensor = 10;
-       mydata.reading = lightvalueNew;
-
-       //send the data
-       ET.sendData();
-       
-     }
-  }
+  
+  checkMotion();
+  
+  timer.run();
+  
   
 
+}
+
+
+
+//void sendData(String message) {
+// 
+//     mydata.sensor = 0;
+//     mydata.reading = 0;
+//
+//    int len = message.length()+1;
+//    
+//    if (len>50)
+//    {
+//      len = 50;
+//    }
+//    
+//    message.toCharArray(mydata.message, 50);
+//     
+//     Serial.print("sending serial message: ");
+//     Serial.println(mydata.message);
+//     
+//     //send the data
+//     ET.sendData(); 
+//  
+//}
+
+void sendData(byte sensor, int reading) {
+ 
+     mydata.sensor = sensor;
+     mydata.reading = abs(reading);
     
+    mydata.readingNegative = reading<0;
+    
+//    for (int i=0;i<50;i++) {
+//         mydata.message[i] = 0;
+//    }
+
+     //send the data
+     ET.sendData(); 
+  
+}
+
+
+void checkMotion() {
+  
   
   boolean motionvalue = digitalRead(motionpin) == HIGH;
   
@@ -126,13 +163,8 @@ void loop() {
      ledOnAt = millis();
    
      Serial.println("w 9 0");
-     //mySerial.println("w 9 0");
 
-     mydata.sensor = 9;
-     mydata.reading = 0;
-
-     //send the data
-     ET.sendData();
+     sendData(9,0);
       
   }
   else if (lastMotion && !motionvalue) {
@@ -141,36 +173,85 @@ void loop() {
      //mySerial.println("w 9 1");    
      digitalWrite(ledpin, LOW);
      
-     
-     mydata.sensor = 9;
-     mydata.reading = 1;
-
-     //send the data
-     ET.sendData();
+     sendData(9,1);
   }
   
   lastMotion = motionvalue;
   
-
-  if (temperatureAt+temperatureInterval <= millis()) {
-      
-    temperatureAt = millis();
-    
-    readTemperatures();
-    
-  }
-
-  //readTemperature(false);
-  //printStatus();
-  
-  
-
 }
 
+void checkLightLevelBig() {
+  
+  checkLightLevel(100);
+  
+}
 
+void checkLightLevelSmall() {
+  
+  checkLightLevel(10);
+  
+}
+
+void checkLightLevel(int minChange) {
+  
+  Serial.print("checkLightLevel ");
+
+  int lightvalueNew = int(analogRead(lightpin)/1024.0*100)*10;
+
+  Serial.println(lightvalueNew);
+    
+//    memcpy(&lightvals[0], &lightvals[1], (9)*sizeof(*lightvals));
+//    lightvals[9] = lightvalueNew;
+//
+//    
+//    for (int x=0;x<10;x++) {
+//      
+//      Serial.print(lightvals[x]);
+//      Serial.print('\t');
+//    }
+//    
+//    Serial.println();
+  
+  
+  
+  
+  int lightChange = abs(lightvalueNew-lightvalue);
+  //only if changed by >= 10
+  if (lightChange>=minChange) {
+
+     delay(50);
+     lightvalueNew = int(analogRead(lightpin)/1024.0*100)*10;
+  
+      lightChange = abs(lightvalueNew-lightvalue);
+      //only if changed by >= 10
+      if (lightChange>=minChange) {
+      
+       //and at most every 10 seconds or > 100 change
+       //if (lightChange>=100) 
+//       {
+//         lightvalueAt = millis();
+         lightvalue = lightvalueNew;
+         
+         Serial.print("w 10 ");
+         Serial.println(lightvalueNew);
+         
+         sendData(10,lightvalueNew);
+       //}
+    }
+  }
+
+    
+  
+}
 
 void initTemperatures() {
   
+  Serial.println("initTemperatures");
+
+  pinMode(tempground, OUTPUT);
+  digitalWrite(tempground, LOW);
+    
+  delay(5000);
   
   // Start up the library
   sensors.begin();
@@ -190,9 +271,13 @@ void initTemperatures() {
   if (sensors.isParasitePowerMode()) Serial.println("ON");
   else Serial.println("OFF");
   
+  
+  
   // Loop through each device, print out address
   for(int i=0;i<numberOfDevices; i++)
   {
+    //temperature[i]=-100;
+    
     // Search the wire for address
     if(sensors.getAddress(tempDeviceAddress, i))
 	{
@@ -202,27 +287,35 @@ void initTemperatures() {
 //		printAddress(tempDeviceAddress);
 		Serial.println();
 		
-		Serial.print("Setting resolution to ");
-		Serial.println(TEMPERATURE_PRECISION, DEC);
+		//Serial.print("Setting resolution to ");
+		//Serial.println(TEMPERATURE_PRECISION, DEC);
 		
 		// set the resolution to TEMPERATURE_PRECISION bit (Each Dallas/Maxim device is capable of several different resolutions)
-		sensors.setResolution(tempDeviceAddress, TEMPERATURE_PRECISION);
+		//sensors.setResolution(tempDeviceAddress, TEMPERATURE_PRECISION);
 		
-		 Serial.print("Resolution actually set to: ");
-		Serial.print(sensors.getResolution(tempDeviceAddress), DEC); 
-		Serial.println();
+		// Serial.print("Resolution actually set to: ");
+		//Serial.print(sensors.getResolution(tempDeviceAddress), DEC); 
+		//Serial.println();
+
+
+             
 	}else{
+  
+  
 		Serial.print("Found ghost device at ");
 		Serial.print(i, DEC);
 		Serial.print(" but could not detect address. Check power and cabling");
+
+             
 	}
   }
   
 }
 
 
-void readTemperatures() {
+void checkTemperatures() {
   
+  Serial.println("checkTemperatures");
   
   // call sensors.requestTemperatures() to issue a global temperature 
   // request to all devices on the bus
@@ -234,30 +327,44 @@ void readTemperatures() {
   // Loop through each device, print out temperature data
   for(int i=0;i<numberOfDevices; i++)
   {
-    
+         
+    //String msg = "checking temp for sensor ";
+    //msg += i;
+    //sendData(msg); 
+     
     float fahrenheit = sensors.getTempFByIndex(i);
+    
+      
     // Search the wire for address
     if(fahrenheit != DEVICE_DISCONNECTED_F)
     {
       
-      if (abs(temperature[i]-fahrenheit)>=1) {
+      //if (abs(temperature[i]-fahrenheit)>=1) {
     
           temperature[i] = fahrenheit;
        
          Serial.print("w ");
          Serial.print(i+tempDeviceStart);
          Serial.print(" ");
-         Serial.println(fahrenheit);
+         Serial.println(int(fahrenheit));
          
-         
-         mydata.sensor = i+tempDeviceStart;
-         mydata.reading = int(fahrenheit);
-    
-         //send the data
-         ET.sendData();
+          sendData(i+tempDeviceStart, int(fahrenheit));
              
-      }
-    } 
+//      }
+//      else {
+//       
+//    
+//     sendData("temp unchanged"); 
+//       //Serial.println("temp unchanged");     
+//        
+//      }
+    }
+    else {
+      
+      Serial.println("Temp Sensor disconnected");
+     //sendData("Temp Sensor disconnected"); 
+      
+    }
     //else ghost device! Check your power requirements and cabling
 	
   }
